ce8b7dd3e8ecaa4513516737ad6f3f5cbb240ffd
荆兴业 <1193345101@qq.com>
2024-01-12 13:25:55 +0800

    测试3

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,9 @@ public class Main {
     public static void main(String[] args) {
         int i =10;
         i=i++;
+        ++i;
         System.out.println("Hello world!");
         System.out.println(i);
+
     }
 }
